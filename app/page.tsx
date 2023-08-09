e0b4631d3f7b613bf2eeeeb5794dd6031ba6b835
Gaurav Chandrakar | GC <34914837+gauravchandrakar25@users.noreply.github.com>
2023-08-09 23:55:31 +0530

    pages added

--- app/page.tsx
+++ app/page.tsx
@@ -11,28 +11,28 @@ export default function Home() {
           <br></br>
         </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none mt-5">
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-          <svg
-            className="w-6 h-6 text-black-800 dark:text-grey"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 16 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1.707 2.707 5.586 5.586a1 1 0 0 0 1.414 0l5.586-5.586A1 1 0 0 0 13.586 1H2.414a1 1 0 0 0-.707 1.707Z"
-            />
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg
+              className="w-6 h-6 text-black-800 dark:text-grey"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1.707 2.707 5.586 5.586a1 1 0 0 0 1.414 0l5.586-5.586A1 1 0 0 0 13.586 1H2.414a1 1 0 0 0-.707 1.707Z"
+              />
 
-            {/* <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /> */}
-          </svg>
-          <span>Start here</span>
-        </button></div>
+              {/* <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /> */}
+            </svg>
+            <span>Start here</span>
+          </button>
+        </div>
       </div>
-
     </main>
   );
 }
